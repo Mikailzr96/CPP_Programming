041b8ed37632559a4e3b300b6e417502fff6ddb5
Mikailzr96 <75627147+Mikailzr96@users.noreply.github.com>
2021-04-10 20:14:38 +0300

    Basic C++

---
+++ Referanslar - Call by Referance/Referanslar - Call by Referance.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <locale>
+using namespace std;
+
+void c_swap(int* p1, int* p2) {
+	int temp = *p1;
+	*p1 = *p2;
+	*p2 = temp;
+}
+void r_swap(int& r1, int& r2) {
+	int temp = r1;
+	r1 = r2;
+	r2 = temp;
+}
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+
+	int x = 7, y = 9;
+	/*--------------------------------------------------------------------------------------------------------------------*/
+	cout << "Pointer ile sayı değiştirme işlemi -->\nX = " << x << "\t Y = " << y << endl;
+
+	c_swap(&x, &y);
+
+	cout << "X = " << x << "\t Y = " << y << endl;
+	/*--------------------------------------------------------------------------------------------------------------------*/
+	cout << "Referans ile sayı değiştirme işlemi -->\nX = " << x << "\t Y = " << y << endl;
+
+	r_swap(x, y);
+
+	cout << "X = " << x << "\t Y = " << y << endl;
+	/*--------------------------------------------------------------------------------------------------------------------*/
+}
+
---
+++ Referanslar/Referanslar.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <locale>
+using namespace std;
+
+/*
+	C++'da, referans değişken adı verilen bir değişken tanımlayarak oluşturduğumuz değişenlere farklı bir isim ile erişim sağlayabiliriz.
+
+	Referans bildirimi aşağıdaki şekilde yapılmaktadır:
+		veri-türü& referans-adı = değişken-adı;
+
+	-Referans değişkenlerinin bildirimi yapıldığında bir ilk değer verilmelidir. Ancak, bir sınıf içinde yer aldığında, bir fonksiyon
+	parametresi veya bir fonksiyon geri dönüş değeri olarak kullanıldığında bu kural geçerli değildir.
+
+*/
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+
+
+	int id;
+	int& rid = id; // Referans tanımı
+
+	/*
+	Referans oluşturulduğunda, bellekte herhangi bir değişiklik olmaz. Yani, rid referans değerini depolamak için bellek kullanılmaz.
+	Referans rid aslında id değişkeni için ikinci bir isim olarak kullanılır. Bu durumda, referans değeri kullandığımızda değişken adını
+	kullanmış gibi oluruz. id değerini değiştirmek rid değerini değiştirmek ve rid değerini değiştirmek id değerini değiştirmek anlamına gelir.
+	*/
+
+	id = 21;
+	cout << id << "\t" << rid << endl;
+
+	rid = 142;
+	cout << id << "\t" << rid << endl;
+
+
+}
